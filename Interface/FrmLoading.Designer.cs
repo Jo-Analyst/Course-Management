10c787e0142b8c3a627b3544b6007ef201f951b6
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2025-07-17 13:16:32 -0300

    link

--- Interface/FrmLoading.Designer.cs
+++ Interface/FrmLoading.Designer.cs
@@ -77,9 +77,11 @@
             // lbClose
             // 
             this.lbClose.AutoSize = true;
-            this.lbClose.Location = new System.Drawing.Point(761, 253);
+            this.lbClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbClose.LinkColor = System.Drawing.Color.White;
+            this.lbClose.Location = new System.Drawing.Point(732, 244);
             this.lbClose.Name = "lbClose";
-            this.lbClose.Size = new System.Drawing.Size(59, 20);
+            this.lbClose.Size = new System.Drawing.Size(88, 29);
             this.lbClose.TabIndex = 6;
             this.lbClose.TabStop = true;
             this.lbClose.Text = "Fechar";
